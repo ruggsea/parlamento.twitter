aac26e37e5030b6f4d3b6cd022fdd50dffb46fd9
ruggsea <874453@unive.it>
2021-08-21 02:36:56 +0200

    ue

--- quanteda.r
+++ quanteda.r
@@ -20,12 +20,21 @@ summary(corpustweet,1)
 kwic(corpustweet,pattern= "negr*")
 
 stp=stopwords(language = "it")
-tk=tokens(corpustweet) %>% 
+tk=tokens(corpustweet,
+          remove_punct = TRUE, 
+          remove_numbers = TRUE,
+          remove_symbols = TRUE,
+          remove_url = TRUE,
+          padding = TRUE) %>% 
   tokens_remove(stp,padding = TRUE)
 
 coll <- textstat_collocations(tk, tolower = FALSE, size = 3)
 
+corpdfm <- dfm(tk)
 
+featnames(corpdfm)
+textstat_frequency(corpdfm, 
+                   n = 1, groups = screen_name)
 summary(corpus_subset(corpustweet, names(corpustweet) =="text21438"))
 
 
